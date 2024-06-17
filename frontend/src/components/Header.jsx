c60ca78e941d70fe99b83527a227f4bb226e4783
blueonline07 <ldkhang1201@gmail.com>
2024-06-17 23:31:12 +0700

    add dockerfile

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,8 @@
 import axios from 'axios'
 function Header() {
-  function getLocation(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(getHere)
-    }
-    else{
-      console.log('geolocation is not supported')
-    }
-  }
-  async function getHere(location){
-    const resp = await axios.get(`https://geocode.search.hereapi.com/v1/geocode?at=${location.coords.latitude},${location.coords.longitude}&q=quan com&in=countryCode:VNM&apiKey=${import.meta.env.VITE_APIKEY}`)
-    console.log(resp.data)
-  }
-  
   return (
     <header>
-      <h1 onClick={getLocation}>Keeper</h1>
+      <h1>Keeper</h1>
     </header>
   );
 }
